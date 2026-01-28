c374dfea89dcee1b9a0432a12e1433782cb3177a
Maxi Gregorutti <maxigregorutti24@gmail.com>
2026-01-28 11:02:49 -0300

    fix: Configure API baseURL to http://localhost:3000 directly

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+﻿import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
+  baseURL: 'http://localhost:3000', // 'http://localhost:3000',
 });
 
 api.interceptors.request.use((config) => {
